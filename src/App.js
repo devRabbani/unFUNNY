89b65983bd31d6dea65dd55dc2228e278cafa528
unknown <gitusertester@gmail.com>
2022-10-28 22:08:30 +0545

    changed fetching url

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
-import React, { lazy, Suspense, useState } from "react";
-import "./App.css";
-import { Switch, Route, withRouter } from "react-router-dom";
-import Buttons from "./components/Buttons/buttons.component";
-import SkeletonCard from "./skeleton/skeletonCard";
-import SkeletonMain from "./skeleton/skeletonMain";
-import SkeletonCardSmall from "./skeleton/skeletoncardsmall";
+import React, { lazy, Suspense, useState } from 'react'
+import './App.css'
+import { Switch, Route, withRouter } from 'react-router-dom'
+import Buttons from './components/Buttons/buttons.component'
+import SkeletonCard from './skeleton/skeletonCard'
+import SkeletonMain from './skeleton/skeletonMain'
+import SkeletonCardSmall from './skeleton/skeletoncardsmall'
 
-const MainJokes = lazy(() => import("./pages/mainJokes/mainJokes.component"));
+const MainJokes = lazy(() => import('./pages/mainJokes/mainJokes.component'))
 const SecondJokes = lazy(() =>
-  import("./pages/secondJokes/secondJokes.component")
-);
+  import('./pages/secondJokes/secondJokes.component')
+)
 
 const App = ({ location: { pathname } }) => {
-  const [jokes1, setJokes1] = useState("");
-  const [jokes2, setJokes2] = useState("");
-  const [catg, setCatg] = useState("Any");
-  const [btnLoading, setBtnLoading] = useState(false);
+  const [jokes1, setJokes1] = useState('')
+  const [jokes2, setJokes2] = useState('')
+  const [catg, setCatg] = useState('Any')
+  const [btnLoading, setBtnLoading] = useState(false)
 
   const fetchJokeV1 = async () => {
-    setBtnLoading(true);
-    const res = await fetch(`https://v2.jokeapi.dev/joke/${catg}?type=single`);
-    const data = await res.json();
-    setJokes1(data);
-    setBtnLoading(false);
-  };
+    setBtnLoading(true)
+    const res = await fetch(`https://v2.jokeapi.dev/joke/${catg}?type=single`)
+    const data = await res.json()
+    setJokes1(data)
+    setBtnLoading(false)
+  }
   const fetchJokeV2 = async () => {
-    setBtnLoading(true);
-    const res = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
-    const data = await res.json();
-    setJokes2(data);
-    setBtnLoading(false);
-  };
+    setBtnLoading(true)
+    const res = await fetch('https://icanhazdadjoke.com/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+    const data = await res.json()
+    setJokes2(data)
+    setBtnLoading(false)
+  }
 
   return (
-    <div className="App">
-      {pathname === "/achhawala" ? (
-        <h1 className="nowfunny">
+    <div className='App'>
+      {pathname === '/achhawala' ? (
+        <h1 className='nowfunny'>
           nowFUNNY!
           {btnLoading ? <span>&#128534;</span> : <span>&#128520;</span>}
         </h1>
@@ -47,7 +52,7 @@ const App = ({ location: { pathname } }) => {
 
       {/* <Switch> */}
       <Suspense fallback={<SkeletonMain />}>
-        <Route exact path="/">
+        <Route exact path='/'>
           <MainJokes
             fetchJoke={fetchJokeV1}
             jokes={jokes1}
@@ -58,7 +63,7 @@ const App = ({ location: { pathname } }) => {
         </Route>
       </Suspense>
       <Suspense fallback={<SkeletonCardSmall />}>
-        <Route exact path="/achhawala">
+        <Route exact path='/achhawala'>
           <SecondJokes
             fetchJoke={fetchJokeV2}
             jokes={jokes2}
@@ -68,11 +73,11 @@ const App = ({ location: { pathname } }) => {
       </Suspense>
       {/* </Switch> */}
       <Buttons
-        fetchJoke={pathname === "/achhawala" ? fetchJokeV2 : fetchJokeV1}
-        isSecPath={pathname === "/achhawala"}
+        fetchJoke={pathname === '/achhawala' ? fetchJokeV2 : fetchJokeV1}
+        isSecPath={pathname === '/achhawala'}
       />
     </div>
-  );
-};
+  )
+}
 
-export default withRouter(App);
+export default withRouter(App)
